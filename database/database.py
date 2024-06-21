14d902dc2bbe18f1bf362e4150b9df42a52b58c5
Yash126152 <161591104+Yash126152@users.noreply.github.com>
2024-06-21 13:34:55 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,21 +1,15 @@
-#(©)CodeXBotz
+import pymongo
 
+# Define the MongoDB URI and database name
+DB_URI = "mongodb+srv://yash14:Yash25@cluster0.qhyz4vx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+DB_NAME = "cluster0"
 
-
-
-import pymongo, os
-from config import DB_URI, DB_NAME
-
-
+# Initialize MongoDB client
 dbclient = pymongo.MongoClient(DB_URI)
 database = dbclient[DB_NAME]
-
-
 user_data = database['users']
 
-
-
-async def present_user(user_id : int):
+async def present_user(user_id: int):
     found = user_data.find_one({'_id': user_id})
     return bool(found)
 
@@ -28,7 +22,6 @@ async def full_userbase():
     user_ids = []
     for doc in user_docs:
         user_ids.append(doc['_id'])
-        
     return user_ids
 
 async def del_user(user_id: int):
